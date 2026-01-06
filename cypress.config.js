4ba00de4774b907036d8eded8a8137ea30643a75
Utada-san <134759891+RamaDutasmara@users.noreply.github.com>
2026-01-06 21:04:23 +0700

    Create cypress.config.js

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  e2e: {
+    baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    setupNodeEvents(on, config) {
+      // node events jika dibutuhkan
+    },
+  },
+  video: false,
+};
